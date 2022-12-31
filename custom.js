5b2d17b5dd3af59f460e9b55fedb67ee165f0921
giladwo <25708271+giladwo@users.noreply.github.com>
2022-12-31 21:34:38 +0200

    Force LTR for latex (normal and inline)

--- custom.js
+++ custom.js
@@ -1,10 +1,22 @@
-const observer = new top.MutationObserver(() => {
-  const allElems = top?.document.querySelectorAll(".ls-block");
-  if (allElems && allElems.length > 0) {
-    for (const elem of allElems) {
-      elem.setAttribute("dir", "auto");
+const setAttributeBySelector = attribute => value => querySelector => {
+  const elements = top?.document.querySelectorAll(querySelector);
+  if (elements && elements.length > 0) {
+    for (const element of elements) {
+      element.setAttribute(attribute, value);
     }
   }
+};
+
+const setDir = setAttributeBySelector("dir");
+const setAuto = setDir("auto");
+const setLTR = setDir("ltr");
+
+const observer = new top.MutationObserver(() => {
+  setAuto(".ls-block");
+  setAuto(".block-properties > div");
+  setLTR(".katex");
+  setLTR(".latex-inline");
+  setAuto(".is-paragraph");
 });
 
 observer.observe(top.document.getElementById("app-container"), {
